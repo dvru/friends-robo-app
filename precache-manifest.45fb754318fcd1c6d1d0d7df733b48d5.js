f90ea0e0f87557292a58f9dab956311e486b89c8
dvru <damvaru4@gmail.com>
2020-04-15 20:00:03 -0500

    Updates

---
+++ precache-manifest.45fb754318fcd1c6d1d0d7df733b48d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba29f37cee1f9880250dbc132c284c4c",
+    "url": "/dvru/friends-robo-app/index.html"
+  },
+  {
+    "revision": "e689d68312b2f66cec7e",
+    "url": "/dvru/friends-robo-app/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "2b34f3a5acfa4e5d2369",
+    "url": "/dvru/friends-robo-app/static/css/main.3bcc3ee4.chunk.css"
+  },
+  {
+    "revision": "e689d68312b2f66cec7e",
+    "url": "/dvru/friends-robo-app/static/js/2.9986c3ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dvru/friends-robo-app/static/js/2.9986c3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b34f3a5acfa4e5d2369",
+    "url": "/dvru/friends-robo-app/static/js/main.c6d1e4f0.chunk.js"
+  },
+  {
+    "revision": "999530e701bada7c74da",
+    "url": "/dvru/friends-robo-app/static/js/runtime-main.9c9da7d4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/dvru/friends-robo-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
